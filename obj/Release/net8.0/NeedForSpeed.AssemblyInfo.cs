371fffd478bbe86ba3c8249c8f5f848aeacaa58e
mahmoodn04 <40336250+mahmoodn04@users.noreply.github.com>
2024-04-29 18:44:06 +0200

    Final

--- obj/Release/net8.0/NeedForSpeed.AssemblyInfo.cs
+++ obj/Release/net8.0/NeedForSpeed.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("NeedForSpeed")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1377cfbb297a49ede0f13396ca87c570cf70d71")]
 [assembly: System.Reflection.AssemblyProductAttribute("NeedForSpeed")]
